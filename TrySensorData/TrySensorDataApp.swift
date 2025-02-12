0b4bc92764eb0c95cf52ff210a612b9d04285324
Naif <naifdev@gmail.com>
2025-02-12 14:46:56 +0300

    Initial Commit

---
+++ TrySensorData/TrySensorDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrySensorDataApp.swift
+//  TrySensorData
+//
+//  Created by Naif Alrashed on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrySensorDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrySensorDataTests/TrySensorDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrySensorDataTests.swift
+//  TrySensorDataTests
+//
+//  Created by Naif Alrashed on 12/02/2025.
+//
+
+import Testing
+@testable import TrySensorData
+
+struct TrySensorDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
